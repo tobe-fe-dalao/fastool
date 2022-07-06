8b969412212ce7d2730f60d952c82eaf546c5b69
bindmonk <934951243@qq.com>
2022-07-06 17:25:59 +0800

    🐞 fix: 修复打包路径问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,14 @@
 
-module.exports={
+module.exports = {
   lang: 'en-US',
   title: 'FasTool',
+  base: './',
   description: '一个短小而精悍的现代JavaScript使用工具库',
   lastUpdated: true,
   head: [
     // 添加图标
     ['link', { rel: 'icon', href: '/favicon.ico' }]
-],
+  ],
   themeConfig: {
     nav: nav(),
     sidebar: {
@@ -19,7 +20,7 @@ module.exports={
     ],
 
   }
-  
+
 }
 
 function nav() {
@@ -32,28 +33,28 @@ function nav() {
 }
 
 function sidebarGuide() {
- return [
-  {
-    items: [
-      { text: 'Storage操作', link: '/guide/Storage' },
-      { text: 'String操作', link: '/guide/String' },
-      { text: 'Time操作', link: '/guide/Time' },
-      { text: 'Fn函数', link: '/guide/Fn' },
-      { text: 'Random操作', link: '/guide/Random' },
-      { text: 'Regex校验', link: '/guide/Regex' },
-      { text: 'URL链接操作', link: '/guide/URL' },
-      { text: 'Device设备', link: '/guide/Device' },
-      { text: 'Browser浏览器', link: '/guide/Browser' },
-      { text: 'Number操作', link: '/guide/Number' },
-      { text: 'Cookie操作', link: '/guide/Cookie' },
-      { text: 'Media操作', link: '/guide/Media' },
-      { text: 'Polyfill兼容', link: '/guide/Polyfill' },
-      { text: 'Canvas操作', link: '/guide/Canvas' },
-      { text: 'Event事件', link: '/guide/Event' },
-      { text: 'Plugins插件库', link: '/guide/Plugins' },
-      { text: 'Tools工具库', link: '/guide/Tools' },
-    ]
-  },
- ]
+  return [
+    {
+      items: [
+        { text: 'Storage操作', link: '/guide/Storage' },
+        { text: 'String操作', link: '/guide/String' },
+        { text: 'Time操作', link: '/guide/Time' },
+        { text: 'Fn函数', link: '/guide/Fn' },
+        { text: 'Random操作', link: '/guide/Random' },
+        { text: 'Regex校验', link: '/guide/Regex' },
+        { text: 'URL链接操作', link: '/guide/URL' },
+        { text: 'Device设备', link: '/guide/Device' },
+        { text: 'Browser浏览器', link: '/guide/Browser' },
+        { text: 'Number操作', link: '/guide/Number' },
+        { text: 'Cookie操作', link: '/guide/Cookie' },
+        { text: 'Media操作', link: '/guide/Media' },
+        { text: 'Polyfill兼容', link: '/guide/Polyfill' },
+        { text: 'Canvas操作', link: '/guide/Canvas' },
+        { text: 'Event事件', link: '/guide/Event' },
+        { text: 'Plugins插件库', link: '/guide/Plugins' },
+        { text: 'Tools工具库', link: '/guide/Tools' },
+      ]
+    },
+  ]
 }
 
