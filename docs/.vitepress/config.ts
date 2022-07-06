2cc03e6a011fbd9f09002e302d51084ff5dd612f
bindmonk <934951243@qq.com>
2022-07-06 19:57:02 +0800

    📃 docs: 提供文档案例

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 
 module.exports = {
-  lang: 'en-US',
+  lang: 'zh-CN',
   title: 'FasTool',
   base: '/fastool/',
   description: '一个短小而精悍的现代JavaScript使用工具库',
@@ -12,20 +12,31 @@ module.exports = {
   themeConfig: {
     nav: nav(),
     sidebar: {
-      '/guide/': sidebarGuide(),
+      '/guide/': sidebarGuide()
     },
-    smoothScroll: true,
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/tobe-fe-dalao' }
+      { icon: 'github', link: 'https://github.com/tobe-fe-dalao/fastool/' }
     ],
-
+    editLink: {
+      pattern: 'https://github.com/tobe-fe-dalao/fastool/tree/doc/docs/:path',
+      text: '在GitHub编辑此页'
+    },
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present BlindMonk'
+    },
+    algolia: {
+      appId: 'ZZNDXJ2XIU',
+      apiKey: 'a6e9af943b8057a410b4059a1bb3306f',
+      indexName: 'FasTool'
+    },
   }
 
 }
 
 function nav() {
   return [
-    { text: '我的个人网站', link: 'http://www.tobe-fe-dalao.cn' },
+    { text: '介绍', link: '/guide/' },
     { text: '掘金', link: 'https://juejin.cn/user/3016715636842622' },
     { text: '加入我们', link: 'https://github.com/tobe-fe-dalao/fastool' },
 
@@ -35,26 +46,32 @@ function nav() {
 function sidebarGuide() {
   return [
     {
-      items: [
-        { text: 'Storage操作', link: '/guide/Storage' },
-        { text: 'String操作', link: '/guide/String' },
-        { text: 'Time操作', link: '/guide/Time' },
-        { text: 'Fn函数', link: '/guide/Fn' },
-        { text: 'Random操作', link: '/guide/Random' },
-        { text: 'Regex校验', link: '/guide/Regex' },
-        { text: 'URL链接操作', link: '/guide/URL' },
-        { text: 'Device设备', link: '/guide/Device' },
-        { text: 'Browser浏览器', link: '/guide/Browser' },
-        { text: 'Number操作', link: '/guide/Number' },
-        { text: 'Cookie操作', link: '/guide/Cookie' },
-        { text: 'Media操作', link: '/guide/Media' },
-        { text: 'Polyfill兼容', link: '/guide/Polyfill' },
-        { text: 'Canvas操作', link: '/guide/Canvas' },
-        { text: 'Event事件', link: '/guide/Event' },
-        { text: 'Plugins插件库', link: '/guide/Plugins' },
-        { text: 'Tools工具库', link: '/guide/Tools' },
+      text: '介绍', items: [
+        { text: '介绍', link: '/guide/' },
+        { text: '快速上手', link: '/guide/start' },
       ]
     },
+    {
+      text: '本地存储', items: [
+        { text: 'Storage操作', link: '/guide/Storage' },
+        { text: 'Cookie操作', link: '/guide/Cookie' },
+      ]
+    },
+    // { text: 'String操作', link: '/guide/String' },
+    // { text: 'Time操作', link: '/guide/Time' },
+    // { text: 'Fn函数', link: '/guide/Fn' },
+    // { text: 'Random操作', link: '/guide/Random' },
+    // { text: 'Regex校验', link: '/guide/Regex' },
+    // { text: 'URL链接操作', link: '/guide/URL' },
+    // { text: 'Device设备', link: '/guide/Device' },
+    // { text: 'Browser浏览器', link: '/guide/Browser' },
+    // { text: 'Number操作', link: '/guide/Number' },
+    // { text: 'Media操作', link: '/guide/Media' },
+    // { text: 'Polyfill兼容', link: '/guide/Polyfill' },
+    // { text: 'Canvas操作', link: '/guide/Canvas' },
+    // { text: 'Event事件', link: '/guide/Event' },
+    // { text: 'Plugins插件库', link: '/guide/Plugins' },
+    // { text: 'Tools工具库', link: '/guide/Tools' },
   ]
 }
 
--- docs/guide/Storage.md
+++ docs/guide/Storage.md
@@ -1,2 +1,66 @@
-# Storage
+# LocalStorage
+## setLocalStorage
+设置localStorage
+```typescript
+/**
+ * @func setLocalStorage
+ * @param {string} key
+ * @param {string} value
+ * @returns {void}
+ * @desc  设置localStorage
+ */
+```
+## getLocalStorage
+获取localStorage
+```typescript
+/**
+ * @func getLocalStorage
+ * @param {string} key
+ * @returns {string}
+ * @desc  获取localStorage
+ */
+```
+
+## delLocalStorage
+删除localStorage
+```typescript
+/**
+ * @func delLocalStorage
+ * @param {string} key
+ * @returns {any}
+ * @desc  删除localStorage
+ */
+```
+# SessionStorage
+## setSessionStorage
+设置sessionStorage
+```typescript
+/**
+ * @func setSessionStorage
+ * @param {string} key
+ * @param {string} value
+ * @returns {any}
+ * @desc  设置sessionStorage
+ */
+```
+## getSessionStorage
+获取sessionStorage
+```typescript
+/**
+ * @func getSessionStorage
+ * @param {string} key
+ * @returns {any}
+ * @desc  获取sessionStorage
+ */
+```
+## delSessionStorage
+删除sessionStorage
+```typescript
+/**
+ * @func delSessionStorage
+ * @param {string} key
+ * @returns {any}
+ * @desc  删除sessionStorage
+ */
+```
 
