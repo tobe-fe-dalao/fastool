b76bc1deb634c4331b580bef3ca376908d2e346e
bindmonk <934951243@qq.com>
2022-07-06 22:23:23 +0800

    📃 docs: 提供文档编辑教程

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -49,6 +49,8 @@ function sidebarGuide() {
       text: '介绍', items: [
         { text: '介绍', link: '/guide/' },
         { text: '快速上手', link: '/guide/start' },
+        { text: '参与编辑', link: '/guide/EditMd' },
+
       ]
     },
     {
