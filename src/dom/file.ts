6f52ee9476327bc19f98d23e0de050061a1ce1c3
李苏晨 <lisuchen002@163.com>
2022-07-08 18:09:04 +0800

    feat: 新增文件流转换方法

--- src/dom/file.ts
+++ src/dom/file.ts
@@ -1,10 +1,10 @@
 /**
  * @func dataURLtoFile
- * @param {string} dataUrl 
- * @param {string} fileName 
- * @desc  将base64转换为文件
- * @returns {any}
- * @example dataURLtoFile(dataUrl, fileName)
+ * @param {string} dataUrl
+ * @param {string} fileName
+ * @desc  将dataUrl转换为文件
+ * @return {any} 文件对象
+ * @example let file = dataURLtoFile(dataUrl, fileName)
  */
 export const dataURLtoFile = (dataUrl: string, fileName: string = getFileName()): any => {
   let arr: Array<any> = dataUrl.split(',');
@@ -17,7 +17,23 @@ export const dataURLtoFile = (dataUrl: string, fileName: string = getFileName())
   }
   return new File([u8arr], fileName, { type: mime });
 }
-
+/**
+ * @func base64toFile
+ * @param {string} base64
+ * @param {string} fileName
+ * @desc  将base64转换为文件
+ * @returns {any} 文件对象
+ * @example let file = base64toFile(base64, fileName)
+ */
+export const base64toFile = (base64: string, fileName: string = getFileName()): any => {
+  return dataURLtoFile(base64, fileName)
+}
+/**
+ * @func getFileName
+ * @desc 设置文件名字
+ * @return { string } fileName 文件名
+ * @example let fileName = getFileName();
+ */
 function getFileName() {
   let date = new Date();
   let fileName = '' + date.getFullYear() + (date.getMonth() < 9 ? +'0' : '')
@@ -25,6 +41,13 @@ function getFileName() {
     + date.getHours() + date.getMinutes() + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
   return fileName;
 }
+/**
+ * @func dataURLtoBlob
+ * @param { string } dataurl
+ * @desc 将dataUrl/base64转换为blob
+ * @return { Blob } Blob
+ * @example let blob = dataURLtoBlob(dataurl)
+ */
 export const dataURLtoBlob = (dataurl: string): Blob | null => {
   let mime;
   let u8arr;
@@ -41,9 +64,32 @@ export const dataURLtoBlob = (dataurl: string): Blob | null => {
       while (n--) {
         u8arr[n] = bstr.charCodeAt(n);
       }
-      return new Blob([ u8arr ], { type: mime });
+      return new Blob([u8arr], { type: mime });
     }
   }
   return null;
 };
-
+/**
+ * @func base64toBlob
+ * @param { string } base64
+ * @desc 将base64转换为blob
+ * @return { Blob } Blob
+ * @example let blob = base64toBlob(dataurl)
+ */
+export const base64toBlob = (base64: string): Blob | null => {
+  return dataURLtoBlob(base64)
+};
+/**
+ * @func blobtoFile
+ * @param { Blob } blob
+ * @param { string } fileName
+ * @desc 将blob转换为文件
+ * @return { any } 文件对象
+ * @example let file = blobtoFile(blob, filename)
+ */
+export const blobtoFile = (blob: Blob, fileName: string = getFileName()): any => {
+  return new File([blob], fileName, { type: blob.type })
+};
+export const filetoBase64 = (file: any): string => {
+  return 'aasd'
+}
